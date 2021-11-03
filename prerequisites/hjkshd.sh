ac2b40f641f73572489c90a4331f263ba0e479c2
Yonathan Zerbib <50784530+yoni702@users.noreply.github.com>
2021-11-03 08:44:25 +0200

    ddd

--- prerequisites/hjkshd.sh
+++ prerequisites/hjkshd.sh
@@ -3,8 +3,10 @@ sudo docker container rm yoni_site
 sudo docker volume rm hackeru_volume
 
 
-sudo docker volume create --name hackeru_volume  --opt device=text-files 
+#sudo docker volume create --name hackeru_volume --opt type=none --opt device=/home/devops/docker/text-files/ --opt o=bind
+sudo docker volume create --name volume --opt type=none --opt device=text-files/ --opt o=bind
 sudo docker build -t yoni_site .
 
-sudo docker run -d -p 82:80 --name=yoni_site  --mount --opt type=nfs source=hackeru_volume,destination=/var/www/html yoni_site
-sudo docker run -d -p 80:80 --name yoni_site --mount source=hackeru_volume,target=/var/www/html yoni_site
+#sudo docker run -d -p 82:80 --name=yoni_site  --mount source=hackeru_volume,target=/var/www/html,readonly yoni_site
+sudo docker run -d -p 80:80 --name yoni_site --mount --opt type=nfs source=hackeru_volume,target=/var/www/html yoni_site
+sudo docker run -d -p 82:80 --name=yoni_site --mount source=hackeru_volume,target=/var/www/html yoni_site
